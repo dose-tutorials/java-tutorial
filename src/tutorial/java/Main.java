027b246b17835e42c50f4bb96ca9c615cfe3c892
Jose Luis Perez <perezmarquez9403@gmail.com>
2023-05-10 20:16:21 -0400

    String and escape sequences classes added

--- src/tutorial/java/Main.java
+++ src/tutorial/java/Main.java
@@ -1,5 +1,6 @@
 package tutorial.java;
 
+import java.net.SocketOption;
 import java.util.Date;
 import java.awt.*;
 
@@ -11,7 +12,7 @@ public class Main {
 
         long subscribers = 3_123_456_789L; // longs needs to have L as suffix
 
-        float price =10.99F; // floats need to have F as suffix
+        float price = 10.99F; // floats need to have F as suffix
 
         char letter = 'A'; // single characters should be represented with single quotes
 
@@ -19,8 +20,9 @@ public class Main {
 
         System.out.println("Hello World");
         referenceTypes();
+        strings();
+        escapeSequences();
     }
-
     public static void referenceTypes() {
         Date now = new Date(); // the new keyword allocates memory to store the object
         System.out.println(now);
@@ -29,6 +31,27 @@ public class Main {
         Point point2 = point1; // it will only pass the address to the new variable
     }
     public static void strings() {
+        String message = new String("Hello World"); // Strings in java are reference types
+        String messageShortHand = "Hello World"; // this is a shorthand to avoid call the new keyword
 
+//        message.endsWith("!!"); checks if it ends with
+//        message.startsWith("!!"); check if it starts with
+//        message.length(); return the string length
+//        message.indexOf("H"); returns the index of the first occurrence
+//        message.replace("!", "*"); returns a new string with the specified character replaced
+//        message.toLowerCase(); converts the string to lower case
+//        message.toUpperCase(); converts the string to upper case
+//        message.trim(); trim white spaces after and before the string
+        System.out.println(messageShortHand);
+    }
+    public static void escapeSequences() {
+        String sentence = "Hello \"Luis\""; // the \ character will escape special characters
+        System.out.println(sentence);
+        String path = "c:\\Windows\\..."; // will add \ to the string
+        System.out.println(path);
+        String newLine = "Hi\nI'm Luis"; // will insert a new line in the string
+        System.out.println(newLine);
+        String data = "Name\tAge\tGender"; // will a tab in the string
+        System.out.print(data);
     }
 }
