e5c039cbe0b195e14656c8a6cb86243ef4861a47
joseluis9403 <perezmarquez9403@gmail.com>
2023-07-02 19:36:55 +0400

    Advance up to foor loops lesson

---
+++ src/tutorial/java/ForLoops.java
@@ -0,0 +1,13 @@
+package tutorial.java;
+
+public class ForLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            System.out.println("Hello world" + i);
+        }
+
+        for (int i = 5; i > 0; i--) {
+            System.out.println("Hello world" + i);
+        }
+    }
+}
